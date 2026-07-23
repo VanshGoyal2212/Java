f5f20a619055eaae33c44b1acdbb0a5a6d8d242d
VanshGoyal212 <vansh.25b01010052@abes.ac.in>
2026-07-23 16:13:46 +0530

    Basics

---
+++ Main.java
@@ -0,0 +1,29 @@
+public class Main
+{
+	static int add(int x,int y)
+	{
+		return x+y;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+
+	int a;
+	int b;
+	a=10;
+	b=20;
+	System.out.println(a+b);
+	System.out.println(a-b);
+	System.out.println(a*b);
+
+	int arr[]={1,2,3,4,5};
+	for(int i=0;i<5;i++)
+	{
+		System.out.println(arr[i]);
+	}
+
+	System.out.println(add(a,b));
+
+}
+}
