6dbd3311f6aa1aad2f9b8830b5d31c57c6b11287
VanshGoyal212 <vansh.25b01010052@abes.ac.in>
2026-07-29 22:30:16 +0530

    .

---
+++ vansh.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main() 
+{
+   class A
+   {
+      public:
+         void display()
+         {
+            cout << "Hello World";
+         }
+   };
+   A obj;
+   obj.display();
+}
